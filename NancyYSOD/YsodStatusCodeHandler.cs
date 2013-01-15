6c9a8ab8955c7b152ff9301a9bc20862364ef48a
Graham Hay <grahamrhay@gmail.com>
2013-01-15 17:58:32 +0000

    Added YSOD error handler

---
+++ NancyYSOD/YsodStatusCodeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nancy;
+using Nancy.ErrorHandling;
+
+namespace NancyYSOD
+{
+    public class YsodStatusCodeHandler : IStatusCodeHandler
+    {
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.InternalServerError;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            object errorObject;
+            context.Items.TryGetValue(NancyEngine.ERROR_EXCEPTION, out errorObject);
+            var exception = errorObject as Exception;
+
+            if (exception != null)
+            {
+                throw exception;
+            }
+        }
+    }
+}
